f840f2b872286a86fbf9f1c341e1b6387987811a
mogata <ms46.dev@gmail.com>
2021-12-08 22:52:52 +0900

    storeの値を取得して画面に表示する

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,21 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div class="alert alert-primary">
+    <h1>{{ data.title }}</h1>
+    <!-- $storeはstoreに保存している値のオブジェクト。$store.state.値 でアクセスできる -->
+    <p class="mt-3 h5">{{ $store.state.message }}</p>
+  </div>
 </template>
 
 <script>
+import { ref, reactive } from 'vue'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
+  setup(props) {
+    const data = reactive({
+      title: 'Vuex',
+    })
     return {
-      count: 0
+      data
     }
   }
 }
