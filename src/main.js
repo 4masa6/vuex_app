2a2adf4c9232c9a218f0cb44eac7e6c58c11a109
mogata <ms46.dev@gmail.com>
2021-12-08 22:49:48 +0900

    storeをアプリケーションに組み込む

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { store } from './store.js' // storeをインポート
 
-createApp(App).mount('#app')
+createApp(App).use(store).mount('#app') // アプリケーションに組み込み
