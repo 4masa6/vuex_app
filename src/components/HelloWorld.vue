678a05a89dec6c561f93077c81cb16f1897361ab
mogata <ms46.dev@gmail.com>
2021-12-09 00:17:46 +0900

    mutationをコンポーネントで使用する

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,15 @@
     <h1>{{ data.title }}</h1>
     <!-- $storeはstoreに保存している値のオブジェクト。$store.state.値 でアクセスできる -->
     <p class="mt-3 h5">{{ $store.state.message }}</p>
+    <hr>
+    <div class="btn btn-secondary" @click="$store.commit('count')">
+      <a class="h5">
+        clicked: {{ $store.state.counter }}
+      </a>
+    </div>
+    <button class="btn btn-info" @click="$store.commit('reset')">
+      reset
+    </button>
   </div>
 </template>
 
